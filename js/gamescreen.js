c76d34749e04fd5c748d77bcae0f6e744815ff3a
noobmaster440 <saadsiddiqui440@gmail.com>
2021-07-13 18:33:38 -0400

    added shootin and correct

--- js/gamescreen.js
+++ js/gamescreen.js
@@ -1,12 +1,13 @@
 let currDiv = 3;
 let correctColumn;
 let correctAnswer;
+let ninjas
 const main = () => {
   document.getElementById(
     `psec${currDiv}`
   ).innerHTML = `<img src="../assets/alien.png" alt="alien" />`;
 
-  let ninjas = document.querySelectorAll(".enemylimit");
+  ninjas = document.querySelectorAll(".enemylimit");
   let speed = new Array(5);
   for (let i = 0; i < 5; i++) {
     speed[i] = Math.floor(Math.random() * 10);
@@ -46,6 +47,24 @@ const generateQues = () => {
     correctAnswer;
 };
 
+const showFire = () => {
+  // const cannonStop = document.querySelector(`#psec${currDiv}`).getBoundingClientRect().left
+  // // let ufos = document.querySelectorAll(".ufo .img-objects")
+  // const ufoStop = ninjas[currDiv].getBoundingClientRect().right
+  // let div = document.createElement("div")
+  // div.setAttribute("class", "gunshot")
+  // div.style.width = `${cannonStop - ufoStop}px`
+  // const right = document.querySelector(`#psec${currDiv}`).getBoundingClientRect().right
+  // const left = document.querySelector(`#psec${currDiv}`).getBoundingClientRect().left
+  // div.style.marginLeft = `${(right-left)/2}px`
+  // console.log(div)
+  // document.querySelector(`#psec${currDiv}`).appendChild(div)
+  // ninjas[currDiv-1].style.backgroundColor="orange"
+  ninjas[currDiv-1].setAttribute("class", "gunshot")
+
+}
+
+
 const keyPressed = (event) => {
   if (event.keyCode === 40) {
     document.getElementById(`psec${currDiv}`).innerHTML = ``;
@@ -70,6 +89,22 @@ const keyPressed = (event) => {
     document.querySelectorAll(".playersec h3")[currDiv-1].innerText =
       correctAnswer;
     console.log("arrow up pressed");
+  }
+  else if(event.keyCode==32){
+    console.log(currDiv)
+    console.log(correctColumn)
+
+    showFire()
+    setTimeout(function(){
+      if(currDiv===(correctColumn+1)){
+        alert("you won")
+      }else if(currDiv!==(correctColumn+1)){
+        alert("You lost")
+      }
+
+    }, 100); 
+    
+
   }
 };
 
