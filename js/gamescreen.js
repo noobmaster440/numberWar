21101666c00cbe5ba59ebc81d87b1435eee3f755
noobmaster440 <saadsiddiqui440@gmail.com>
2021-07-13 02:16:16 -0400

    added moving enemies

--- js/gamescreen.js
+++ js/gamescreen.js
@@ -1,14 +1,73 @@
+let currDiv = 3;
 const main = () => {
-  console.log(document.querySelectorAll("div>img"));
-  let ninjas=document.querySelectorAll("div>img");
-  for (let i = 0; ninjas[i].style.marginLeft<1300; i++) {
-      let tempMargin=ninjas[i].style.marginLeft
-     let marginRandom=((Math.random())*1000)+tempMargin;
-     console.log(marginRandom)
+  document.getElementById(`enemy${currDiv}`).innerHTML = `
+  <div class="enemylimit">
+  <div class="visual" id="enemy1">
+    <img src="../assets/ninja.png" alt="ninja" />
+  </div>
+  <div class="player">
+    <img src="../assets/alien.png" alt="alien" />
+  </div>
+</div>
+  `;
+  let ninjas = document.querySelectorAll(".visual");
+  let speed = new Array(5);
 
-      ninjas[i].style.marginLeft=`${marginRandom}px`
-      
+  for (let i = 0; i < 5; i++) {
+    speed[i] = Math.floor(Math.random() * 10);
   }
 
+  setInterval(() => {
+    ninjas.forEach((ninja, i) => {
+      speed[i] += Math.random() * 10;
+      ninja.style.marginLeft = `${speed[i]}%`;
+    });
+  }, 1000);
+  window.addEventListener("keydown", keyPressed);
 };
+
+const keyPressed = (event) => {
+  if (event.keyCode === 40) {
+    document.getElementById(`enemy${currDiv}`).innerHTML = `
+    <div class="enemylimit">
+      <div class="visual">
+        <img src="../assets/ninja.png" alt="ninja" />
+      </div>
+    </div>
+    `;
+    currDiv += 1;
+    document.getElementById(`enemy${currDiv}`).innerHTML = `
+    <div class="enemylimit">
+        <div class="visual">
+          <img src="../assets/ninja.png" alt="ninja" />
+        </div>
+        <div class="player">
+          <img src="../assets/alien.png" alt="alien" />
+        </div>
+    </div>
+    `;
+  } else if (event.keyCode === 38) {
+    document.getElementById(`enemy${currDiv}`).innerHTML = `
+    <div class="enemylimit">
+      <div class="visual">
+        <img src="../assets/ninja.png" alt="ninja" />
+      </div>
+    </div>
+    `;
+
+    currDiv -= 1;
+    document.getElementById(`enemy${currDiv}`).innerHTML = `
+    <div class="enemylimit">
+        <div class="visual">
+          <img src="../assets/ninja.png" alt="ninja" />
+        </div>
+        <div class="player">
+          <img src="../assets/alien.png" alt="alien" />
+        </div>
+    </div>
+    `;
+    console.log("arrow up pressed");
+  }
+};
+
 main();
