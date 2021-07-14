890a12297963e2e3e3d752623ec0047958cee5f2
Muzammil <muzammil56@Muzammils-MacBook-Air.local>
2021-07-14 00:49:23 -0400

    index and rules updated

---
+++ js/index.js
@@ -0,0 +1,59 @@
+//variables
+let isMuted
+
+//loads the audio
+const loadAudio = () => {
+    if (localStorage.hasOwnProperty("audio")) {
+        let audio = JSON.parse(localStorage.getItem("audio"))
+        document.querySelector("audio").currentTime = audio.audioTime      
+        if (audio.isMuted) {
+            isMuted = true
+            document.querySelector("audio").muted = true 
+            document.querySelector(".img-audio").src = "../assets/mute.png"
+        }  
+        else {
+            isMuted = false
+            document.querySelector("audio").muted = false 
+            document.querySelector(".img-audio").src = "../assets/volume.png"
+        }
+    }
+    else {
+        isMuted = false
+        document.querySelector("audio").muted = false 
+        document.querySelector(".img-audio").src = "../assets/volume.png"
+    }
+}
+
+//updates the audio
+const updateAudio = () => {
+    let audio = {
+        audioTime: document.querySelector("audio").currentTime,
+        isMuted: isMuted
+    }
+    localStorage.setItem("audio", JSON.stringify(audio))
+}
+
+//functions on load event of the page
+loadAudio()
+  
+//click listeners
+//click event of the audio icon
+document.querySelector(".img-audio").addEventListener("click", () => {
+    if (isMuted === false) {
+        document.querySelector(".img-audio").src = "../assets/mute.png"
+        isMuted = true
+        document.querySelector("audio").muted = true        
+    }
+    else {
+        document.querySelector(".img-audio").src = "../assets/volume.png"
+        isMuted = false
+        document.querySelector("audio").muted = false
+    }
+    updateAudio()
+})
+
+//click event when rules button is pressed
+document.querySelector("#btn-rules").addEventListener("click", () => {
+    window.location.replace("/html/rules.html")
+    updateAudio()
+})
